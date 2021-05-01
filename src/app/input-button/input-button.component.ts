f535702b7420e53af70fbebc5ed747d1e5243d63
bliitzen <chfrance15@gmail.com>
2021-05-01 14:52:42 -0400

    added some styling so pretty

--- src/app/input-button/input-button.component.ts
+++ src/app/input-button/input-button.component.ts
@@ -3,11 +3,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-input-button',
   template: `
-    <input #inputElementRef
+    <input class="todo-input"
+           #inputElementRef
            [value]="title"
            (keyup.enter)="submitValue($any($event.target).value)">
 
-    <button (click)="submitValue(inputElementRef.value)">
+    <button class="btn"
+            (click)="submitValue(inputElementRef.value)">
       Save
     </button>
   `,
--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -4,7 +4,10 @@ import { TodoItem } from '../interfaces/todo-item';
 @Component({
   selector: 'app-todo-item',
   template: `
+  <div class="todo-item">
     {{ item.title }}
+  </div>
+
   `,
   styleUrls: ['./todo-item.component.css']
 })
